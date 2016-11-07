b8b6d98a62ba8f59692617e3cd0a2724fc1943c3
Walfie <walfington@gmail.com>
2016-11-06 23:22:57 -0500

    More specific request/response types

--- src/main/scala/walfie/grailbirds/protocol/package.scala
+++ src/main/scala/walfie/grailbirds/protocol/package.scala
@@ -3,3 +3,37 @@ package walfie.grailbirds.protocol
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
+@ScalaJSDefined
+trait ClientMessage extends js.Object {
+  val streamId: Int
+  val requestType: Int
+  val data: js.Any
+}
+
+object ClientMessage {
+  object RequestType {
+    val Search = 0
+  }
+
+  @ScalaJSDefined
+  trait Request extends js.Object
+
+  @ScalaJSDefined
+  trait Search extends Request {
+    val regex: String
+  }
+}
+
+@ScalaJSDefined
+trait WorkerMessage extends js.Object {
+  val streamId: Int
+  val data: js.Any
+}
+
+object WorkerMessage {
+  def apply(_streamId: Int, _data: js.Any): WorkerMessage = new WorkerMessage {
+    val streamId = _streamId
+    val data = _data
+  }
+}
+
--- src/main/scala/walfie/grailbirds/worker/Worker.scala
+++ src/main/scala/walfie/grailbirds/worker/Worker.scala
@@ -2,8 +2,11 @@ package walfie.grailbirds.worker
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation._
+import scala.scalajs.js.JSConverters._
 import org.scalajs.dom.raw._
 import walfie.grailbirds.domain._
+import walfie.grailbirds.protocol._
+import scala.concurrent.duration._
 
 @JSExport("GrailbirdsWorker")
 object Worker {
@@ -18,12 +21,22 @@ class Worker(scope: DedicatedWorkerGlobalScope, baseUrl: String) {
   val grailbird = new Grailbird(scope, baseUrl)
 
   scope.onmessage = { e: MessageEvent =>
-    val searchTerm = e.data.asInstanceOf[String]
-    val regex = js.RegExp(searchTerm, "im")
+    val message = e.data.asInstanceOf[ClientMessage]
 
-    grailbird.allTweetsObservable
-      .filter(tweet => regex.test(tweet.text))
-      .foreach(tweet => scope.postMessage(tweet))
+    message.requestType match {
+      case ClientMessage.RequestType.Search =>
+        val req = message.data.asInstanceOf[ClientMessage.Search]
+
+        val regex = js.RegExp(req.regex, "im")
+
+        grailbird.allTweetsObservable
+          .filter(tweet => regex.test(tweet.text))
+          .bufferTimedAndCounted(100.millis, 10)
+          .foreach { tweets =>
+            val out = WorkerMessage(message.streamId, tweets.toJSArray)
+            scope.postMessage(out)
+          }
+    }
   }
 }
 
